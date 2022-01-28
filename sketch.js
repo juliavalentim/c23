b5796855b86a5cea4a6a2b0f4fdad9bffe5fcd48
juliavalentim <95942826+juliavalentim@users.noreply.github.com>
2022-01-28 16:20:46 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,9 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var canvas;
 var palyer, playerBase, playerArcher;
-var arrow;
 var baseimage;
-var playerimage;
 
 function preload() {
   backgroundImg = loadImage("./assets/background.png");
@@ -28,44 +26,22 @@ function setup() {
     isStatic: true
   };
 
-  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  PlayerBase = Bodies.rectangle(200, 350, 180, 150,  options);
   World.add(world, playerBase);
 
-  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  World.add(world,player)
+  player = Bodies.rectangle(250, playerBase.position.y,160, 50, 180, options);
+  World.add(world, player)
 
-  playerArcher = new PlayerArcher(
-    340,
-    playerBase.position.y - 112,
-    120,
-    120
-  );
-
-  arrow = new PlayerArrow(
-    playerArcher.body.position.x,
-    playerArcher.body.position.y,
-    100,
-    10
-  );
 }
 
 function draw() {
   background(backgroundImg);
-  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
-  image(playerimage,player.position.x,player.position.y,50,180)
   Engine.update(engine);
 
-  playerArcher.display();
-  arrow.display();
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
 
-  if (keyCode === 32) {
-    //arrow.(playerArcher.body.angle);
-     arrow.shoot(playerArcher.angle);
-    // arrow.shoot(playerArcher.body.angle);
-    // arrow.shoot(playerArcher);
-  }
-
-  // Título
+  // Title
   fill("#FFFF");
   textAlign("center");
   textSize(40);
